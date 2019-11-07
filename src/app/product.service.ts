b30af61e18cea1d8640a03fa284aecc7bce0c519
zeelz <keme.kenneth@gmail.com>
2019-11-07 18:12:24 +0100

    5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,9 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-	private _albumUrl: string;
+	private _albumUrl: string = "../assets/album.json";
 
-	constructor(private _http: Http) { 
-		this._albumUrl = "../assets/album.json";
-	}
+	constructor(private _http: Http) { }
 	
 	getAlbum(id: number){
 		return this._http.get(this._albumUrl).map(response => response.json());
